8da893c19f5875be6632a4e2d276689da7c6b68c
Iva <104653667+pin3dev@users.noreply.github.com>
2025-06-06 14:50:51 -0300

    chore: frontend Docker PORT and name updated, and SSL certificates upload. Frontend serving by 443, sucessfully

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import path from "path";
 
+let httpsConfig = false;
+
+if (process.env.SSL_KEY_BASE64 && process.env.SSL_CERT_BASE64) {
+  const key = Buffer.from(process.env.SSL_KEY_BASE64, 'base64').toString('utf-8');
+  const cert = Buffer.from(process.env.SSL_CERT_BASE64, 'base64').toString('utf-8');
+  httpsConfig = { key, cert };
+}
+
 export default defineConfig({
   server: {
-    port: 3000,
-    host: '0.0.0.0'
+    port: 443,
+    host: '0.0.0.0',
+    https: httpsConfig,
   },
   resolve: {
     alias: {
