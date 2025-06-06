89e80b226171e515ac63849a8a628e5715dc938f
Iva <104653667+pin3dev@users.noreply.github.com>
2025-06-06 17:50:07 -0300

    refactor: general configuration to static frontend builder, vite build not working

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite';
 import path from "path";
 
-let httpsConfig = false;
-
-if (process.env.SSL_KEY_BASE64 && process.env.SSL_CERT_BASE64) {
-  const key = Buffer.from(process.env.SSL_KEY_BASE64, 'base64').toString('utf-8');
-  const cert = Buffer.from(process.env.SSL_CERT_BASE64, 'base64').toString('utf-8');
-  httpsConfig = { key, cert };
-}
-
 export default defineConfig({
-  server: {
-    port: 443,
-    host: '0.0.0.0',
-    https: httpsConfig,
+  // server: {
+  //   port: 443,
+  //   host: '0.0.0.0',
+  // },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
   },
   resolve: {
     alias: {
