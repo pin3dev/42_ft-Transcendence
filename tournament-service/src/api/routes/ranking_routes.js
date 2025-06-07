b2be15d6b5852389cf934ddd5b4ce5a6ca372efe
Iva <104653667+pin3dev@users.noreply.github.com>
2025-06-07 01:03:54 -0300

    feat: new route to get tournament friend stats, working

--- tournament-service/src/api/routes/ranking_routes.js
+++ tournament-service/src/api/routes/ranking_routes.js
@@ -1,6 +1,7 @@
 const { getTopRanking_controller } = require("../controllers/getTopRanking_controller");
 const { getUserStats_controller } = require("../controllers/getUserStats_controller");
-const { getTopRanking_schema, getUserStats_schema } = require("../schemas/ranking_schemas");
+const { getFriendStats_controller } = require("../controllers/getFriendStats_controller");
+const { getTopRanking_schema, getUserStats_schema, getFriendStats_schema } = require("../schemas/ranking_schemas");
 
 module.exports = async function (fastify) {
   fastify.get("/tournament/ranking/top", {
@@ -11,5 +12,10 @@ module.exports = async function (fastify) {
   fastify.get("/tournament/ranking/me", {
     schema: getUserStats_schema,
     handler: getUserStats_controller,
+  });
+
+  fastify.get("/tournament/ranking/user", {
+    schema: getFriendStats_schema,
+    handler: getFriendStats_controller,
   });
 };
