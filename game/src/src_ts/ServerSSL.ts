1fbbc185f5ad9d5c858cd5241e71dd21a4f3d7e8
phrxn <david@quazzom.com>
2025-06-14 20:01:57 -0300

    feat: add only wss to api game server

--- game/src/src_ts/ServerSSL.ts
+++ game/src/src_ts/ServerSSL.ts
@@ -1,6 +1,4 @@
 import * as https from 'https';
-import * as path from 'path';
-import * as fs from 'fs';
 import { WebSocket, WebSocketServer } from 'ws';
 import { WebSocketUserSession } from './WebSocketUserSession';
 
@@ -14,8 +12,8 @@ export class ServerSSL {
 	private SSLcert: string;
 	private JWTpublicKey: string;
 
-	constructor(paratersVariables: ParametersVariables, SSLkey: string, SSLcert: string, JWTpublicKey: string) {
-		this._parametersVariables = paratersVariables;
+	constructor(parametersVariables: ParametersVariables, SSLkey: string, SSLcert: string, JWTpublicKey: string) {
+		this._parametersVariables = parametersVariables;
 		this.SSLkey = SSLkey;
 		this.SSLcert = SSLcert;
 		this.JWTpublicKey = JWTpublicKey;
@@ -24,71 +22,36 @@ export class ServerSSL {
 	public startServer() {
 
 		const api = new API(this.JWTpublicKey);
-
 		const clients = new Map<WebSocket, WebSocketUserSession>();
 
-		const server = https.createServer({
+		const wssServer = https.createServer({
 			key: this.SSLkey,
 			cert: this.SSLcert
-		}
-			, (req, res) => {
+		});
 
-				let urlPath = req.url ?? '/'; // Se req.url for undefined, assume '/'
-				let filePath = path.join(__dirname, '../public', urlPath === '/' ? 'index.html' : urlPath);
-				let ext = path.extname(filePath);
-				let contentType = 'text/html';
-
-				switch (ext) {
-					case '.js': contentType = 'application/javascript'; break;
-					case '.css': contentType = 'text/css'; break;
-				}
-
-				fs.readFile(filePath, (err, content) => {
-					if (err) {
-						res.writeHead(404);
-						res.end('Not Found');
-						return;
-					}
-					res.writeHead(200, { 'Content-Type': contentType });
-					res.end(content);
-				});
-			});
-
-		const wssServer = https.createServer({ key: this.SSLkey, cert: this.SSLcert }); // outro servidor
 		const wss = new WebSocketServer({ server: wssServer });
 
 		wss.on('connection', (ws) => {
 
-			clients.set(ws, new WebSocketUserSession(ws));
+			const session = new WebSocketUserSession(ws);
+			clients.set(ws, session);
 
 			ws.on('message', (message) => {
-
-				let wsSession = clients.get(ws);
-
-				if (!wsSession) return;
-
-				api.message(wsSession, message);
+				const session = clients.get(ws);
+				if (!session) return;
+				api.message(session, message);
 			});
 
 			ws.on('close', () => {
-
-				let wsSession = clients.get(ws);
-
-				if (!wsSession) return;
-
-				api.close(wsSession);
-
+				const session = clients.get(ws);
+				if (!session) return;
+				api.close(session);
 				clients.delete(ws);
 			});
-
 		});
 
 		wssServer.listen(this._parametersVariables.wssPort, () => {
-			console.log(`WebServer listening on https://localhost:${this._parametersVariables.wssPort}`);
-		});
-
-		server.listen(this._parametersVariables.httpsPort, () => {
-			console.log(`Server listening on https://localhost:${this._parametersVariables.httpsPort}`);
+			console.log(`WSS server listening on https://localhost:${this._parametersVariables.wssPort}`);
 		});
 	}
 }
